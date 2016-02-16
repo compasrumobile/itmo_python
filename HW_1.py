32db404459660e96d693cb17d5ee167ea31a4305
Andrey Lekarev <andlek@yandex.ru>
2016-02-16 22:54:13 +0300

    Second version

--- HW_1.py
+++ HW_1.py
@@ -1,24 +1,21 @@
 # -*- coding: utf-8 -*-
 import os
-a = '.'
-"""
-# Вариант с вводом пути пользователем
-a = input('Введите путь к папке (с двумя обратными слэшами):\n')
-"""
+
+#a = input('Введите путь к папке (с двумя обратными слэшами):\n') # вариант, когда путь вводит пользователь
+a = 'D:\\Google\\Python\\Courses\\Lesson2'
+
 if not os.path.exists(a): # Checking for folder existance
     print('Путь указан некорректно')
 else:
-    b = os.listdir(a)
+    b = os.listdir(a) 
     x = 0
-
     for item in b:
-        
-        if os.path.isfile(item):
-            g = open(item, 'r', encoding='utf-8').read() #Как присвоить g путь файла?
-            
-            if 'python' in g:
+        c = (os.path.join(a, item)) # присвоение переменной абсолютного пути текущего файла или папки 
+        if os.path.isfile(c): # проверка, явлется ли item файлом
+            g = open(c, 'r', encoding='utf-8').read() # открытие на чтение  
+            if 'python' in g: # есть ли в содержимом файла "python"?
                 print(item)
-                open('result.txt', 'a', encoding='utf-8').write(item+'\n')
-                x += g.count('python')
+                open(os.path.join(a,'result.txt'), 'a', encoding='utf-8').write(item+'\n') # записать в result.txt имя файла, содержащего "python"
+                x += g.count('python') # счетчик файлов, содержащих "python"
     print(x)
-    open('result.txt', 'a', encoding='utf-8').write(str(x))
+    open(os.path.join(a,'result.txt'), 'a', encoding='utf-8').write(str(x))
